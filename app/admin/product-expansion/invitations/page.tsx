227348753f3788a9c822bcb3a7e7307545dc81b8
hamzaagency1357 <hamza.agency.sy@gmail.com>
2026-07-31 02:18:05 +0300

    feat(pr101): expose tenant invitation administration

---
+++ app/admin/product-expansion/invitations/page.tsx
@@ -0,0 +1,7 @@
+import TenantInvitationsConsole from "@/components/admin/TenantInvitationsConsole";
+
+export const dynamic = "force-dynamic";
+
+export default function TenantInvitationsPage() {
+  return <TenantInvitationsConsole />;
+}
